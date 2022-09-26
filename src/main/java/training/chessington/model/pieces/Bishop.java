d01d8aae061673454ef702b354fb4475ca3ac1d5
tim-barton96 <tbart@xwt34pl7nl.uk.aexp.com>
2022-09-26 15:04:52 +0100

    Bishop works I think

--- src/main/java/training/chessington/model/pieces/Bishop.java
+++ src/main/java/training/chessington/model/pieces/Bishop.java
@@ -18,17 +18,56 @@ public class Bishop extends AbstractPiece {
 
         List<Move> allowedMoves = new ArrayList<>();
 
+        boolean[] upLeft = {false, false};
+        allowedMoves.addAll(getDiagonalMoves(from, board, upLeft));
+        boolean[] upRight = {true, false};
+        allowedMoves.addAll(getDiagonalMoves(from, board, upRight));
+        boolean[] downLeft = {false, true};
+        allowedMoves.addAll(getDiagonalMoves(from, board, downLeft));
+        boolean[] downRight ={true, true};
+        allowedMoves.addAll(getDiagonalMoves(from, board, downRight));
 
-
-
-        return new ArrayList<>();
+        return allowedMoves;
     }
+
+
+    public List<Move> getDiagonalMoves(Coordinates from, Board board, boolean[] whichDirectionArray) {
+
+        List<Move> lineMoves = new ArrayList<>();
+        boolean isBlocked = false;
+        int row = whichDirectionArray[0]? 1: -1;
+        int col = whichDirectionArray[1]? 1: -1;
+        int counter = 1;
+
+        while (counter < 8 && !isBlocked){
+            Coordinates moveToCoords = new Coordinates(from.getRow() + counter * row, from.getCol() + counter * col);
+            //(upLeft, upLeftBlocked, board)
+            if (isSpaceInBounds(moveToCoords) && (board.isSpaceEmpty(moveToCoords) || isOpponentInSpace(moveToCoords, board))) {
+                lineMoves.add(new Move(from, moveToCoords));
+                if (isOpponentInSpace(moveToCoords, board)) {
+                    isBlocked = true;
+                }
+            } else {
+                isBlocked = true;
+            }
+            counter++;
+        }
+
+        return lineMoves;
+    }
+
+    public boolean isOpponentInSpace(Coordinates coords, Board board) {
+        if (board.isSpaceEmpty(coords)) {
+            return false;
+        } else return !board.get(coords).getColour().equals(this.colour);
+    }
+
 
 
     public boolean isSpaceInBounds(Coordinates coords) {
         if (coords.getRow() < 0 || coords.getRow() > 7) {
             return false;
-        }
-        return coords.getCol() >= 0 && coords.getCol() <= 7;
+        } else return coords.getCol() >= 0 && coords.getCol() <= 7;
     }
+
 }
--- src/test/java/training/chessington/model/pieces/BishopTest.java
+++ src/test/java/training/chessington/model/pieces/BishopTest.java
@@ -1,7 +1,47 @@
 package training.chessington.model.pieces;
 
+import org.junit.Test;
+import training.chessington.model.Board;
+import training.chessington.model.Coordinates;
+import training.chessington.model.Move;
+import training.chessington.model.PlayerColour;
+
+import java.util.List;
+
 import static training.chessington.model.pieces.PieceAssert.*;
 import static org.assertj.core.api.Assertions.*;
 
 public class BishopTest {
+
+    @Test
+    public void bishopCanMoveUpOneLeftOne() {
+        // Arrange
+        Board board = Board.empty();
+        Piece bishop = new Bishop(PlayerColour.WHITE);
+        Coordinates coords = new Coordinates(3, 3);
+        board.placePiece(coords, bishop);
+
+        // Act
+        List<Move> moves = bishop.getAllowedMoves(coords, board);
+
+        // Assert
+        assertThat(moves).contains(new Move(coords, coords.plus(-1, -1)));
+    }
+
+    @Test
+    public void bishopCannotMoveOverPieces() {
+
+        Board board = Board.empty();
+        Piece bishop = new Bishop(PlayerColour.WHITE);
+        Piece pawn = new Pawn(PlayerColour.WHITE);
+        Coordinates coords = new Coordinates(3, 3);
+        board.placePiece(coords, bishop);
+        board.placePiece(coords.plus(1,1), pawn);
+
+        // Act
+        List<Move> moves = bishop.getAllowedMoves(coords, board);
+
+        // Assert
+        assertThat(moves).doesNotContain(new Move(coords, coords.plus(1, 1)));
+    }
 }
